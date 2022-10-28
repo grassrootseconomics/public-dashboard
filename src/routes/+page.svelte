dac3f1836202b0889b3afc8ff78bdbfade12b9ef
christopherreay <christopherreay@gmail.com>
2022-10-28 18:03:38 +0000

    using bootstrap responsive spacing. experimental #9 #13

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,46 @@
 <div role="main">
 
-  <div class="mainHeadingAndSearch">
+  <div class="mainHeadingAndSearch px-3 px-md-4 px-lg-5 py-5">
 
     <h5 class="homePageMainH5">The Grassroots Economics Blockchain Explorer</h5>
 
     <form action="search" method="POST">
       <div class="searchInputGroup input-group input-group-lg">
-        <button class="searchFilterButton btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">filters</button>
+        <button
+          class="searchFilterButton d-none d-sm-block btn btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false">filters</button
+        >
         <ul class="dropdown-menu">
           <li><a class="dropdown-item" href="#">Action</a></li>
           <li><a class="dropdown-item" href="#">Another action</a></li>
           <li><a class="dropdown-item" href="#">Something else here</a></li>
-          <li><hr class="dropdown-divider"></li>
+          <li><hr class="dropdown-divider" /></li>
           <li><a class="dropdown-item" href="#">Separated link</a></li>
         </ul>
-        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-        <button class="btn btn-primary" type="button" id="button-addon2"><iconify-icon class="searchIcon" inline height="1.5em" icon="material-symbols:search"></iconify-icon></button>
+        <input
+          type="text"
+          class="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+        />
+        <button class="btn btn-primary" type="button" id="button-addon2"
+          ><iconify-icon
+            class="searchIcon"
+            inline
+            height="1.5em"
+            icon="material-symbols:search"
+          /></button
+        >
       </div>
     </form>
-    
   </div>
-  
-  
-
-  
-
 </div>
 
 <style>
 .mainHeadingAndSearch {
   height: 256px;
-  width: 100%;
-
-  padding-top: 48px;
-  padding-left: 32px;
-  padding-bottom: 48px;
-  padding-right: 32px;
  
   background-image: url("/graphics/backgroundImages/hexagon.svg");
 }
@@ -43,18 +48,18 @@
 .homePageMainH5 {
   color: white;
 }
+  .searchInputGroup {
+    max-width: 610px;
+}
 
-.searchInputGroup
-{ max-width: 610px 
-}
-@media (max-width: 690px) {
-  .searchFilterButton {
-    display: none; 
-  } 
-}
 .searchIcon {
   margin: -0.4em;
 }
 
-
+  .homePageStatistics {
+    margin-top: -48px;
+    /* border-radius:16px; */
+    /* background-color: white; */
+    /* height: 200px; */
+  }
 </style>
