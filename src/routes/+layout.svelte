6fabf1f8663146a0d240a48cbc7f067c1fb7d5b8
christopherreay <christopherreay@gmail.com>
2022-11-03 19:05:11 +0000

    placed container around all pages to restrict width #3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,30 @@
 
 </script>
 
+<div class="pageWrapper">
 
-<MainSiteNavBar>
-
-
-</MainSiteNavBar>
+  <MainSiteNavBar>
 
 
-<slot></slot>
+  </MainSiteNavBar>
+  
+  
+  <slot></slot>
+
+</div>
+
+<style>
+
+.pageWrapper {
+  height: 100vh;
+  width: 100vw;
+
+  max-width: 1080px;
+
+  margin: 0px auto;
+
+  background-color: rgba(255,255,255, 0.5);
+  box-shadow: 0px 0px 20px 20px rgba(0,0,0,0.1)
+}
+
+</style>
