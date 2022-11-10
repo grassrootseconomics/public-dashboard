8eea27a4cec14e1deb275203714dfbfb6a8f2f3c
christopherreay <christopherreay@gmail.com>
2022-11-10 13:46:19 +0000

    added markdown-it with example usage #7

---
+++ src/routes/developerDocumentation/navTree/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+
+import { onMount } from 'svelte';
+
+let pageDescription = 
+`
+### working on page navigation tooling
+* here we have an example of how to use:
+  * SvelteKit advanced routing: https://kit.svelte.dev/docs/advanced-routing
+  * with the \`load\` tooling to track the current url: https://kit.svelte.dev/docs/load
+  * 
+`;
+
+onMount
+    ( () =>
+      {
+        let MarkdownIt = markdownit();
+
+        pageDescription = MarkdownIt.render(pageDescription);
+      }
+    );
+
+
+
+
+</script>
+
+<div>
+
+  {@html pageDescription}
+
+</div>
