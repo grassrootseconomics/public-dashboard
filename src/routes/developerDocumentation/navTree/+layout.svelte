8141c9baa97173b3f55757a95bc1eb709cce186e
christopherreay <christopherreay@gmail.com>
2022-11-10 13:48:49 +0000

    added basic underlying sveltekit structure on which to build navUtil #7

---
+++ src/routes/developerDocumentation/navTree/+layout.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+
+let navTree = [];
+
+for (let i=0; i<3; i++)
+{ navTree.push({id: i});
+}
+
+</script>
+
+
+
+{#each navTree as numberCounter  }
+
+  <a href="/developerDocumentation/navTree/page{numberCounter.id}">
+    <div id="{numberCounter.id}">{numberCounter.id}</div>
+  </a>
+
+{/each}
+
+<slot></slot>
---
+++ src/routes/developerDocumentation/navTree/page[pageNumber]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+  export let data;
+
+  let pageNumber = data.params.pageNumber;
+
+  console.log(`page[pagenumber=${JSON.stringify(data)}]`);
+
+</script>
+
+<h1>{pageNumber}</h1>
